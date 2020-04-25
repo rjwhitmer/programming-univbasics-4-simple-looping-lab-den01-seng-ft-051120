c655b07f30b3196e1b128492405a79e8f6963632
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-25 19:16:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,11 +16,12 @@ def loop_message_n_times(message, number)
   end
 end
 
-def output_array(message, number)
+def output_array(array)
   counter = 0 
   
-  while counter < number
-    puts [message]
+  while counter < array.length do
+    puts array[counter]
     counter += 1 
   end
+end
 end
